50ad1b080f40f899c66eb0553b3ea58e72a44c14
Andreas Bloch <ablochs@gmail.com>
2019-01-31 21:42:31 +0100

    Remember to export new comp

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 import TreeComponent from './components/TreeComponent.react';
+import Select from './components/Select.react';
 
 export {
-    TreeComponent
+    TreeComponent,
+    Select,
 };
