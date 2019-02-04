8ec11f794d3125332c52c4a1646938c2709ccf9f
Andreas Bloch <ablochs@gmail.com>
2019-02-04 22:02:03 +0100

    Add VarEditor to demo

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,32 +1,45 @@
 /* eslint no-magic-numbers: 0 */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import { TreeComponent } from '../lib';
+import { TreeComponent, VarEditor } from '../lib';
+
 
 class App extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            value: ''
-        };
-        this.setProps = this.setProps.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      showVarEditor: false,
+    };
+    this.setProps = this.setProps.bind(this);
+    this.addVar = this.addVar.bind(this);
+  }
 
-    setProps(newProps) {
-        this.setState(newProps);
-    }
+  setProps(newProps) {
+    this.setState(newProps);
+  }
 
-    render() {
-        return (
-            <div>
-                <TreeComponent
-                    setProps={this.setProps}
-                    {...this.state}
-                />
-            </div>
-        )
-    }
+  addVar() {
+    this.setState({ showVarEditor: !this.state.showVarEditor })
+  }
+
+  render() {
+    return (
+      <div>
+        <TreeComponent
+          setProps={this.setProps}
+          {...this.state}
+          label='hej'
+        />
+        <VarEditor
+          setProps={this.setProps}
+          show={this.state.showVarEditor}
+        />
+        <button onClick={this.addVar}>Add var</button>
+      </div>
+    )
+  }
 }
 
 export default App;
