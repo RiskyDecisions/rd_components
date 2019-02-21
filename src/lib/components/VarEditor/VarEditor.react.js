e7433462787b690233faffe20aa756e53b144b18
Andreas Bloch <ablochs@gmail.com>
2019-02-21 13:19:17 +0100

    Missing TYPE_METHOD_MAP

--- src/lib/components/VarEditor/VarEditor.react.js
+++ src/lib/components/VarEditor/VarEditor.react.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { pick } from 'ramda';
 
 import './VarEditor.css';
-
+import { TYPE_METHOD_MAP } from '../../constants';
 
 const initialState = {
   correlate: false,
